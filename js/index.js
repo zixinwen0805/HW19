c65b571b2845c21dfff0e76b95c09e4cb2a992a8
zixinwen0805 <593877516j@gmail.com>
2022-10-13 20:32:01 +1100

    hw19

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+$(".abt").on("click", function(){
+    $(this).css("background-color", "#437256");
+  });
+  
+  $(".work").on("click", function(){
+    $(this).css("background-color", "#2431AC");
+  });
+  
+  $(".contact").on("click", function(){
+    $(this).css("background-color", "#D15219");
+  });
